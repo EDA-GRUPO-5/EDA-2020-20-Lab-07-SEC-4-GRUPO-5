a3784b825b9c5e8f6c0081ece9340e604d0ec3a3
Laurarestrepo03 <69609680+Laurarestrepo03@users.noreply.github.com>
2020-09-25 20:33:12 -0500

    Update view.py

--- App/view.py
+++ App/view.py
@@ -38,7 +38,7 @@ operación seleccionada.
 # ___________________________________________________
 
 
-crimefile = 'crime-utf8.csv'
+accidentsfile = 'us_accidents_small.csv'
 
 # ___________________________________________________
 #  Menu principal
@@ -51,7 +51,7 @@ def printMenu():
     print("Bienvenido")
     print("1- Inicializar Analizador")
     print("2- Cargar información de accidentes")
-    print("3- Requerimento 1")
+    print("3- Conocer los accidentes en una fecha")
     print("4- Requerimento 2")
     print("0- Salir")
     print("*******************************************")
@@ -71,9 +71,17 @@ while True:
 
     elif int(inputs[0]) == 2:
         print("\nCargando información de crimenes ....")
+        controller.loadData(cont, accidentsfile)
+        print('Accidentes cargados: ' + str(controller.accidentsSize(cont)))
+        print('Altura del arbol: ' + str(controller.indexHeight(cont)))
+        print('Elementos en el arbol: ' + str(controller.indexSize(cont)))
+        print('Menor Llave: ' + str(controller.minKey(cont)))
+        print('Mayor Llave: ' + str(controller.maxKey(cont)))
 
     elif int(inputs[0]) == 3:
-        print("\nBuscando crimenes en un rango de fechas: ")
+        accidentDate = input("Ingrese la fecha: ")
+        print("\nBuscando accidentes de " + str(accidentDate))
+        print(cont["accidents"])
 
 
     elif int(inputs[0]) == 4:
